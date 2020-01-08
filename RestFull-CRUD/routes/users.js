e7958276a26c5151cbe6e0a04c7f56e00c9ec60e
BiscegliaDomenico <bisceglia.domenico@iisgalvanimi.edu.it>
2020-01-08 10:29:06 +0000

    primo

---
+++ RestFull-CRUD/routes/users.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const sql = require('mssql')
+const config = {
+  user: '4DD_07',  //Vostro user name
+  password: 'xxx123##', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  database: '4DD_07', //(Nome del DB)
+}
+
+/* GET users listing. */
+router.get('/autori', function(req, res, next) {
+  sql.connect(config, err => {
+    if(err) console.log(err);  // ... error check
+    
+    // Query
+    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
+    sqlRequest.query('select * from BD_AUTORE', (err, result) => {
+        if (err) console.log(err); // ... error checks
+        res.send(result);  //Invio il risultato
+    });
+  });
+});
+
+module.exports = router;
