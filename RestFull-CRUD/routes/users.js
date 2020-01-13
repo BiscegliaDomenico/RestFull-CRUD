ac522684780385a720526f88f2f05a0ca65d6ed6
BiscegliaDomenico <bisceglia.domenico@iisgalvanimi.edu.it>
2020-01-13 09:41:55 +0000

    royale

--- RestFull-CRUD/routes/users.js
+++ RestFull-CRUD/routes/users.js
@@ -9,16 +9,43 @@ const config = {
 }
 
 /* GET users listing. */
-router.get('/autori', function(req, res, next) {
+router.get('/attributes', function(req, res, next) {
   sql.connect(config, err => {
     if(err) console.log(err);  // ... error check
     
     // Query
     let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-    sqlRequest.query('select * from BD_AUTORE', (err, result) => {
+    sqlRequest.query('select * from [cr-unit-attributes]', (err, result) => {
         if (err) console.log(err); // ... error checks
         res.send(result);  //Invio il risultato
     });
+    
+  });
+});
+router.get('/unit', function(req, res, next) {
+  sql.connect(config, err => {
+    if(err) console.log(err);  // ... error check
+    
+    // Query
+    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
+    sqlRequest.query('select Unit from [cr-unit-attributes]', (err, result) => {
+        if (err) console.log(err); // ... error checks
+        res.send(result);  //Invio il risultato
+    });
+    
+  });
+});
+router.get('/:unit', function(req, res, next) {
+  sql.connect(config, err => {
+    if(err) console.log(err);  // ... error check
+    
+    // Query
+    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
+    sqlRequest.query(`select * from [cr-unit-attributes] where Unit = '${req.params.unit}'`, (err, result) => {
+        if (err) console.log(err); // ... error checks
+        res.send(result);  //Invio il risultato
+    });
+    
   });
 });
 
