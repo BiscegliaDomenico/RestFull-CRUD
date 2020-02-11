3e5efca69aaf341501f148054978818580884f8f
BiscegliaDomenico <bisceglia.domenico@iisgalvanimi.edu.it>
2020-02-11 11:47:10 +0000

    punto1

--- RestFull-CRUD/routes/index.js
+++ RestFull-CRUD/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
-
+const sql = require('mssql');
+const config = {
+  user: '4DD_07',  //Vostro user name
+  password: 'xxx123##', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  database: '4DD_07', //(Nome del DB)
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/images', function(req, res, next) {
+  sql.connect(config, err => {
+    if(err) console.log(err);  // ... error check
+    
+    // Query
+    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
+    sqlRequest.query('select Unit from [cr-unit-attributes]', (err, result) => {
+        if (err) console.log(err); // ... error checks
+        res.render('immagini', {dati:result.recordset}) ;  //Invio il risultato
+    });
+    
+  });
 });
 
 module.exports = router;
